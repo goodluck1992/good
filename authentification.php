2e042a7ca337be6f20695aebe22f62e012305fbb
goodluck1992 <goodluckcommando@gmail.com>
2020-06-15 13:09:00 +0300

    fdahdhdf

---
+++ authentification.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <table width="200" border="1">
+    <tr>
+      <td>Name</td>
+      <td><label for="Name"></label>
+      <input type="text" name="Name" id="Name" /></td>
+    </tr>
+    <tr>
+      <td>Passwaord</td>
+      <td><label for="Password"></label>
+      <input type="password" name="Password" id="Password" /></td>
+    </tr>
+    <tr>
+      <td colspan="2" align="right"><label for="Login"></label>
+      <input type="Login
+      " name="Login" id="Login" value="Login" /></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
---
+++ commando.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	background-color: #0F3;
+}
+</style>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <table width="200" border="0" align="center">
+    <tr>
+      <td>Name</td>
+      <td align="center"><input type="text" name="Name" id="Name" /></td>
+    </tr>
+    <tr>
+      <td>Email</td>
+      <td><input type="text" name="Email" id="Email" /></td>
+    </tr>
+    <tr>
+      <td colspan="2" align="right"><label for="Submit"></label>
+      <input type="submit" name="Submit" id="Submit" value="Submit" /></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
